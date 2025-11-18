ff9ad6aed06f2524a55d439f10670b3384f5cd69
Gabriel Schwanke <gabriellschwanke@gmail.com>
2025-11-18 12:26:56 -0300

    Script do menu responsivo

--- footer.php
+++ footer.php
@@ -12,27 +12,7 @@
         document.querySelector(".menu-toggle").addEventListener("click", function() {
             document.querySelector(".site-nav").classList.toggle("active");
         });
-
-        // --- Animação de aparecer ao rolar (para .orientacoes) ---
-        const orientacoes = document.querySelectorAll('.orientacoes');
-
-    if (orientacoes.length > 0) {
-        const observer = new IntersectionObserver((entries) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    // quando aparece
-                    entry.target.classList.add('visivel');
-                } else {
-                    // quando sai da tela
-                    entry.target.classList.remove('visivel');
-                }
-            });
-        }, {
-            threshold: 0.2, // ativa quando 20% estiver visível
-        });
-
-        orientacoes.forEach(el => observer.observe(el));
-    }
+        
 </script>
 </body>
 </html>
